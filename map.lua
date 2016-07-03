3f3a13f06f666814b6570bd6e232177a09549b01
Vasek Escaron Hodina <wasseker@gmail.com>
2016-07-03 23:00:21 +0200

    Added a pallete that will be used, bagan working on rewrite of view matrix function in map.lua

--- map.lua
+++ map.lua
@@ -1,7 +1,7 @@
 -- map.lua the class with functions and variables uded with maps
-local map = {}
+local Map = {}
 
-map.currentMap = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+Map.currentMap = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     			   {1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
     			   {1,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
              {1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1},
@@ -22,6 +22,53 @@ map.currentMap = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
 		  		   {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1},
              {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}}
 
-map.items = {}
+Map.items = {}
 
-return map
+--Function that provides a matrix that is a part of map, 
+--It takes x,y position on the map, range how big the matrix should be and a direction where it should be progressing
+--It takes x,y as center of one side of square and maps all the tiles in from of it in the specified direction
+function map:getMapSquare(x,y,range,direction)
+	local map_square = {}
+	for i=1,4 do map_sqare[i] = {} end
+	--if direction is to north, we always move the beginning to left bottom of matrix
+	if direction == 0 then
+		x = x - math.floor(x/range)
+		for i=1,range do
+			for j=1,range do
+				if x+j <= 0 or x+j > #Map.currentMap or y-i <= 0 or y-i > #Map.currentMap
+					then map_square[i][j] = 0
+					else map_square[i][j] = Map.currentMap[y-i][x+j]
+			end
+		end
+	end
+	--TODO finish
+	--if direction is to east
+	if direction == 0 then
+		y = y - math.floor(y/range)
+		for i=1,range do
+			for j=1,range do
+
+			end
+		end
+	end
+	--if direction is to south
+	if direction == 0 then
+		x = x + math.floor(x/range)
+		for i=1,range do
+			for j=1,range do
+
+			end
+		end
+	end
+	--if direction is to west
+	if direction == 0 then
+		x = y + math.floor(y/range)
+		for i=1,range do
+			for j=1,range do
+
+			end
+		end
+	end
+
+
+return Map
--- worldScreen.lua
+++ worldScreen.lua
@@ -4,9 +4,11 @@ local WS = {}
 map = require "map"
 Party = require "Party"
 
-local lWallCoords = {{4,4}, {4,24}, {4,68}, {28,108}}
-local rWallCoords = {{900,4}, {900,24}, {900,70}, {876,108}}
-local fWallCoords = {{4,4}, {120,28}, {236,72}, {308,108}}
+local lWallCoords = {{1,1}, {1,10}, {1,20}, {7,29}}
+local rWallCoords = {{225,1}, {225,10}, {225,20}, {219,29}}
+local fWallCoords = {{1,1}, {30,7}, {59,18}, {77,27}}
+local floor_coords = {1,40}
+local ceiling_coords = {1,1}
 local party = 0
 local local_map = 0
 viewMatrix = 0
@@ -21,43 +23,38 @@ end
 
 
 function WS:loadWorldScreen()
-	floor = love.graphics.newImage("assets/world/templates/floor.png")
-	roof = love.graphics.newImage("assets/world//templates/ceiling.png")
+	floor = love.graphics.newImage("assets/world/orig_templates/floor_template.png")
+	roof = love.graphics.newImage("assets/world//orig_templates/ceiling_template.png")
 
-	front_wall1 = love.graphics.newImage("assets/world/templates/front1_template.png")
-	front_wall2 = love.graphics.newImage("assets/world/templates/front2_template.png")
-	front_wall3 = love.graphics.newImage("assets/world/templates/front3_template.png")
+	front_wall1 = love.graphics.newImage("assets/world/orig_templates/front1_template.png")
+	front_wall2 = love.graphics.newImage("assets/world/orig_templates/front2_template.png")
+	front_wall3 = love.graphics.newImage("assets/world/orig_templates/front3_template.png")
 	
-	left_wall1 = love.graphics.newImage("assets/world/templates/side1_template.png")
-	left_wall2 = love.graphics.newImage("assets/world/templates/side2_template.png")
-	left_wall3 = love.graphics.newImage("assets/world/templates/side3_template.png")
-	left_wall4 = love.graphics.newImage("assets/world/templates/side4_template.png")
-
-	lft_wall1 = love.graphics.newImage("assets/world/templates/side1_template.png")
-	lft_wall2 = love.graphics.newImage("assets/world/templates/side2_template.png")
-	lft_wall3 = love.graphics.newImage("assets/world/templates/side3_template.png")
-
+	left_wall1 = love.graphics.newImage("assets/world/orig_templates/side0_template.png")
+	left_wall2 = love.graphics.newImage("assets/world/orig_templates/side1_template.png")
+	left_wall3 = love.graphics.newImage("assets/world/orig_templates/side2_template.png")
+	left_wall4 = love.graphics.newImage("assets/world/orig_templates/side3_template.png")
 end
 
 function WS:drawWorldScreen(SCALE)
-	love.graphics.draw(floor,4,212,0,SCALE)
-	love.graphics.draw(roof,4,4,0,SCALE)
+	love.graphics.draw(floor,floor_coords[1]*SCALE,floor_coords[2]*SCALE,0,SCALE)
+	love.graphics.draw(roof,ceiling_coords[1]*SCALE,ceiling_coords[2]*SCALE,0,SCALE)
 	viewMatrix = WS:createViewMatrix(self.party:getX(), self.party:getY(), self.party:getDirection(), self.local_map)
-	if viewMatrix[4][1] == 1 then love.graphics.draw(left_wall4, lWallCoords[4][1],lWallCoords[4][2],0,SCALE) end			
-	if viewMatrix[4][3] == 1 then love.graphics.draw(left_wall4, rWallCoords[4][1],rWallCoords[4][2],0,-SCALE,SCALE) end			
-	if viewMatrix[4][2] == 1 then love.graphics.draw(front_wall3,fWallCoords[4][1],fWallCoords[4][2],0,SCALE) end			
+	if viewMatrix[4][1] == 1 then love.graphics.draw(left_wall4, lWallCoords[4][1]*SCALE,lWallCoords[4][2]*SCALE,0,SCALE) end			
+	if viewMatrix[4][3] == 1 then love.graphics.draw(left_wall4, rWallCoords[4][1]*SCALE,rWallCoords[4][2]*SCALE,0,-SCALE,SCALE) end			
+	if viewMatrix[4][2] == 1 then love.graphics.draw(front_wall3,fWallCoords[4][1]*SCALE,fWallCoords[4][2]*SCALE,0,SCALE) end			
 
-	if viewMatrix[3][1] == 1 then love.graphics.draw(left_wall3, lWallCoords[3][1],lWallCoords[3][2],0,SCALE) end			
-	if viewMatrix[3][3] == 1 then love.graphics.draw(left_wall3, rWallCoords[3][1],rWallCoords[3][2],0,-SCALE,SCALE) end			
-	if viewMatrix[3][2] == 1 then love.graphics.draw(front_wall2,fWallCoords[3][1],fWallCoords[3][2],0,SCALE) end			
+	if viewMatrix[3][1] == 1 then love.graphics.draw(left_wall3, lWallCoords[3][1]*SCALE,lWallCoords[3][2]*SCALE,0,SCALE) end			
+	if viewMatrix[3][3] == 1 then love.graphics.draw(left_wall3, rWallCoords[3][1]*SCALE,rWallCoords[3][2]*SCALE,0,-SCALE,SCALE) end			
+	if viewMatrix[3][2] == 1 then love.graphics.draw(front_wall2,fWallCoords[3][1]*SCALE,fWallCoords[3][2]*SCALE,0,SCALE) end			
 
-	if viewMatrix[2][1] == 1 then love.graphics.draw(left_wall2, lWallCoords[2][1],lWallCoords[2][2],0,SCALE) end			
-	if viewMatrix[2][3] == 1 then love.graphics.draw(left_wall2, rWallCoords[2][1],rWallCoords[2][2],0,-SCALE,SCALE) end			
-	if viewMatrix[2][2] == 1 then love.graphics.draw(front_wall1,fWallCoords[2][1],fWallCoords[2][2],0,SCALE) end			
+	if viewMatrix[2][1] == 1 then love.graphics.draw(left_wall2, lWallCoords[2][1]*SCALE,lWallCoords[2][2]*SCALE,0,SCALE) end			
+	if viewMatrix[2][3] == 1 then love.graphics.draw(left_wall2, rWallCoords[2][1]*SCALE,rWallCoords[2][2]*SCALE,0,-SCALE,SCALE) end			
+	if viewMatrix[2][2] == 1 then love.graphics.draw(front_wall1,fWallCoords[2][1]*SCALE,fWallCoords[2][2]*SCALE,0,SCALE) end			
 
-	if viewMatrix[1][1] == 1 then love.graphics.draw(left_wall1, lWallCoords[1][1],lWallCoords[1][2],0,SCALE) end			
-	if viewMatrix[1][3] == 1 then love.graphics.draw(left_wall1, rWallCoords[1][1],rWallCoords[1][2],0,-SCALE,SCALE) end			
-end
+	if viewMatrix[1][1] == 1 then love.graphics.draw(left_wall1, lWallCoords[1][1]*SCALE,lWallCoords[1][2]*SCALE,0,SCALE) end			
+	if viewMatrix[1][3] == 1 then love.graphics.draw(left_wall1, rWallCoords[1][1]*SCALE,rWallCoords[1][2]*SCALE,0,-SCALE,SCALE) end			
+end                                                                            
 
 function WS:createViewMatrix(x,y,partyDirection,currentMap)
 	local viewMatrix = {}
