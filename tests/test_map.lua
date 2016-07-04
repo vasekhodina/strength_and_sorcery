e52faca62c64b547b90a8de62372d7ec686d39f5
vasekhodina <wasseker@gmail.com>
2016-07-04 20:02:05 +0200

    Added testing of map:getSquare

--- tests/test_map.lua
+++ tests/test_map.lua
@@ -20,4 +20,6 @@
 package.path = package.path .. ";../?.lua"
 map = require"map"
 printable_map = map:getMapSquare(17,5,5,0)
-print(printable_map)
+for i=1,5 do
+  print(table.concat(printable_map[i]))
+end
